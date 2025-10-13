b5921fab02eebf740eaaa81bdcb95ae8e79820c4
pradanaadn <pradanaadn21@gmail.com>
2025-10-13 15:37:12 +0800

    feat: add Gemini configuration and document processing pipeline with chunking functionality

--- poc_chatbot/backend/document_processing_pipeline.py
+++ poc_chatbot/backend/document_processing_pipeline.py
@@ -1,7 +1,70 @@
-
 import pymupdf4llm
-llama_reader = pymupdf4llm.LlamaMarkdownReader()
-md_text = llama_reader.load_data("assets/example_documents.pdf", show_progress=True)
-for text in md_text:
-    # pathlib.Path("assets/extracted.md").write_text(text)
-    print(text)
+from pydantic import BaseModel, Field
+import uuid
+import hashlib
+import datetime
+
+class DocumentChunkMetadata(BaseModel):
+    page_number: int = Field(ge=1)
+    max_page: int = Field(ge=1)
+    source: str = Field(description="Source file path or URL")
+    subject: str | None = Field(default=None)
+    title: str = Field(default="")
+    keywords: list[str] = Field(default=[])
+    authors: list[str] = Field(default=[])
+
+
+class DocumentChunk(BaseModel):
+    chunk_id: str = Field(description="Unique identifier for the document chunk")
+    text: str = Field(description="Text content of the document chunk")
+    metadata: DocumentChunkMetadata | None = Field(
+        default=None, description="Metadata associated with the document chunk"
+    )
+
+def generate_id(text:str, page_number:int, create_date:datetime.datetime) -> str:
+    return hashlib.sha256(f"{page_number}-{text}-{create_date}".encode()).hexdigest()
+
+class DocumentProcessingPipeline:
+    def __init__(self, source: str):
+        self.source = source
+        self.chunks: list[DocumentChunk] = []
+
+    def load(self):
+        self.load_document = pymupdf4llm.to_markdown(
+            doc=self.source,
+            page_chunks=True,
+            show_progress=True,
+            ignore_alpha=True,
+            ignore_images=True,
+            ignore_graphics=True,
+            force_text=True,
+        )
+
+    def chunk(self)-> list[DocumentChunk]:
+        for page_data in self.load_document:
+            page_markdown = page_data["text"]
+            page_metadata = page_data["metadata"]
+            chunk = DocumentChunk(
+                chunk_id=generate_id(page_markdown, page_metadata.get("page", 0), datetime.datetime.now()),
+                text=page_markdown,
+                metadata=DocumentChunkMetadata(
+                    page_number=page_metadata.get("page", 0),
+                    max_page=page_metadata.get("page_count", 0),
+                    source=self.source,
+                    subject=page_metadata.get("subject", None),
+                    title=page_metadata.get("title", ""),
+                    keywords=list(set(page_metadata.get("keywords", []).split(","))),
+                    authors=list(set(page_metadata.get("author", []).split(","))),
+                    ),
+            )
+            self.chunks.append(chunk)
+        return self.chunks
+    
+
+
+# if __name__ == "__main__":
+#     process = DocumentProcessingPipeline("assets/example_documents.pdf")
+#     process.load()
+#     chunks = process.chunk()
+#     for chunk in chunks:
+#         print(chunk.model_dump())
